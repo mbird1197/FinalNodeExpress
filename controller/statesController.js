2d5f764ad3585c7eb7ec43a87ff3a89f3f922d5a
Mark Bird <m_bird2@mail.fhsu.edu>
2024-04-28 11:40:08 -0700

    u

--- controller/statesController.js
+++ controller/statesController.js
@@ -75,7 +75,7 @@ const getStatePopulation = async (req, res) =>
         return state.code === code;
     });
 
-    res.json({ 'state': stateData.state, 'population': stateData.population });
+    res.json({ 'state': stateData.state, 'population': stateData.population.toString() });
 
 }
 
