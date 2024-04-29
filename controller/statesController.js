6da2d6c4c666eb57795fd153d4189dc2da743a66
Mark Bird <m_bird2@mail.fhsu.edu>
2024-04-28 17:50:32 -0700

    u

--- controller/statesController.js
+++ controller/statesController.js
@@ -94,14 +94,16 @@ const getStateAdmission = async (req, res) =>
 }
 
 
-/*const getFunFact = async (req, res) => {
+const getFunFact = async (req, res) => {
 
     const stateCode = req.code;
+    
   
     const state = await State.findOne({stateCode});
 
-    if(state.funfacts.length === 0 || state.funfacts === undefined){
-        res.status(204).json({message : `No Fun Facts found for Georgia`});
+    if (state.funfacts === undefined || state.funfacts.length === 0) {
+        return res.status(404).json({message : `No Fun Facts found for Georgia`}); // Plain text response
+    
 
     }
         
@@ -143,7 +145,7 @@ const addFunFact = async (req, res) => {
     }
 
 
-*/
+}}
 
 
 module.exports = { 
@@ -153,7 +155,7 @@ module.exports = {
      getStateNickname,
       getStatePopulation,
        getStateAdmission, 
-       //getFunFact,
+       getFunFact,
     //addFunFact 
 };
 
